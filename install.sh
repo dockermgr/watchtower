85bec6e90462631fa055b3c119b21ea3a2fe336b
casjay <git-admin@casjaysdev.pro>
2024-09-24 14:02:49 -0400

    🗃️ Committing everything that changed 🗃️
    install.sh

--- install.sh
+++ install.sh
@@ -547,7 +547,7 @@ __setup_cron() {
   HOST_CRON_ENABLED="yes"
   HOST_CRON_USER="root"
   HOST_CRON_SCHEDULE="30 */6 * * *"
-  HOST_CRON_COMMAND="curl -q -LSsf -H \"Authorization: Bearer ${CONTAINER_API_KEY_TOKEN}\" \"$NGNIX_REVERSE_ADDRESS/v1/update\" >/dev/null 2>&1"
+  HOST_CRON_COMMAND="curl -q -LSsf -H \"Authorization: Bearer ${CONTAINER_API_KEY_TOKEN}\" \"$CONTAINER_SERVER_TEST_URL/v1/update\" >/dev/null 2>&1"
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # enable the health check - creates a cron script - [yes/no] [/health]
@@ -2127,7 +2127,6 @@ SET_EXECUTE_PRE_INSTALL="$(echo "docker stop $CONTAINER_NAME &>/dev/null;docker 
 SET_EXECUTE_DOCKER_CMD="$(echo "docker run -d $DOCKER_GET_OPTIONS $DOCKER_GET_CUSTOM $DOCKER_GET_LINK $DOCKER_GET_LABELS $DOCKER_GET_CAP $DOCKER_GET_SYSCTL $DOCKER_GET_DEV $DOCKER_SET_DNS $DOCKER_GET_ENV $DOCKER_GET_MNT $DOCKER_GET_PUBLISH $DOCKER_HUB_IMAGE_URL:$DOCKER_HUB_IMAGE_TAG")"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Run functions
-__setup_cron
 __container_import_variables "$CONTAINER_ENV_FILE_MOUNT"
 __dockermgr_variables >"$DOCKERMGR_CONFIG_DIR/env/$CONTAINER_NAME.env.conf"
 __custom_docker_script >"$DOCKERMGR_CONFIG_DIR/env/$CONTAINER_NAME.script.sh"
@@ -2506,10 +2505,10 @@ if [ "$CONTAINER_INSTALLED" = "true" ] || __docker_ps_all -q; then
     echo '*/90 * * * * root dockermgr server_status "'$CONTAINER_SERVER_TEST_URL/$HOST_SERVER_HEALTH_CHECK_SERVER_URI'" "'$CONTAINER_NAME'" "'$DOCKERMGR_INSTALL_SCRIPT'"' >"/etc/cron.d/${CONTAINER_NAME}_health"
     printf '# - - - - - - - - - - - - - - - - - - - - - - - - - -\n'
   fi
+  __setup_cron
   if [ "$HOST_CRON_ENABLED" = "yes" ] && [ -n "$HOST_CRON_COMMAND" ]; then
     [ -n "$HOST_CRON_USER" ] || HOST_CRON_USER="root"
     [ -n "$HOST_CRON_SCHEDULE" ] || HOST_CRON_SCHEDULE="30 0 * * *"
-    HOST_CRON_COMMAND="$(eval echo "${HOST_CRON_COMMAND[*]}")"
     __printf_spacing_color "6" "Setting cron user to:" "$HOST_CRON_USER"
     __printf_spacing_color "6" "Setting schedule to:" "$HOST_CRON_SCHEDULE"
     __printf_spacing_color "3" "Saving cron job to: /etc/cron.d/${CONTAINER_NAME}_cron"
